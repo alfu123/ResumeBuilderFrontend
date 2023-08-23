e62c749c8bc530af83014e1d01b94de593631ebf
alfu123 <alfu9721@gmail.com>
2023-08-23 09:44:58 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    emailAddress: '',
+    phoneNo: '',
+    address: '',
+    linkedInAddress: '',
+    imageUrl: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('http://localhost:8090/api/headers', formData);
+      alert('User data submitted successfully!');
+    } catch (error) {
+      alert('An error occurred while submitting user data.');
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Information Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+
+        <label>Email Address:</label>
+        <input
+          type="email"
+          name="emailAddress"
+          value={formData.emailAddress}
+          onChange={handleInputChange}
+        />
+
+        <label>Phone Number:</label>
+        <input
+          type="tel"
+          name="phoneNo"
+          value={formData.phoneNo}
+          onChange={handleInputChange}
+        />
+
+        <label>Address:</label>
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleInputChange}
+        />
+
+        <label>LinkedIn Address:</label>
+        <input
+          type="text"
+          name="linkedInAddress"
+          value={formData.linkedInAddress}
+          onChange={handleInputChange}
+        />
+
+        <label>Image URL:</label>
+        <input
+          type="text"
+          name="imageUrl"
+          value={formData.imageUrl}
+          onChange={handleInputChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
